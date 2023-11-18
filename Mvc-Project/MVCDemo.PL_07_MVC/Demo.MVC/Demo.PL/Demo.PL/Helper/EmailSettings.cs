c38c81efc34385d5aaf421980f610f05d3aed843
Eslam-Mohamed66 <em8418555@gmail.com>
2023-11-19 01:13:44 +0200

    Mvc Project with crud operations

---
+++ Mvc-Project/MVCDemo.PL_07_MVC/Demo.MVC/Demo.PL/Demo.PL/Helper/EmailSettings.cs
@@ -0,0 +1,20 @@
+﻿using Demo.DAL.Entities;
+using System.Net;
+using System.Net.Mail;
+
+namespace Demo.PL.Helper
+{
+    public class EmailSettings
+    {
+        public static void SendEmail(Email  email)
+        {
+            var client = new SmtpClient("smtp.ethereal.email", 587);
+
+            client.EnableSsl = true;
+
+            client.Credentials = new NetworkCredential("terrance41@ethereal.email", "4AAcym6phtS5zXzKJ9");
+
+            client.Send("terrance41@ethereal.email", email.To , email.Title, email.Body);
+        }
+    }
+}
